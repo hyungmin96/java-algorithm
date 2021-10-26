de34b38d259ab1b83c820517fe1e4eaee9c5d309
bana_na21 <bana_na21@naver.com>
2021-10-26 22:49:25 +0900

    자바 기초 알고리즘 공부 19일차

--- DFS/경로탐색/Main/src/Main.java
+++ DFS/경로탐색/Main/src/Main.java
@@ -3,44 +3,46 @@ import java.io.*;
 
 public class Main {
 
-    static int[][] board;
     static boolean[] visited;
+    static int[][] board;
+    static int n, k, answer;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        int n = Integer.parseInt(input.split(" ")[0]);
-        int k = Integer.parseInt(input.split(" ")[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
-        board = new int[n + 1][n + 1];
-        visited = new boolean[n + 1];
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < k; i ++){
-            String str = br.readLine();
-            int row = Integer.parseInt(str.split(" ")[0]);
-            int col = Integer.parseInt(str.split(" ")[1]);
+        visited = new boolean[n + 1];
+        board = new int[n + 1][n + 1];
+
+        for(int i = 1; i <= k; i ++){
+            StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
+            int row = Integer.parseInt(stz.nextToken());
+            int col = Integer.parseInt(stz.nextToken());
             board[row][col] = 1;
         }
 
-        dfs(n);
+        visited[1] = true;
+        dfs(1);
+
+        System.out.println(answer);
     }
 
-    public static int solution(int n, int k, int[][] board){
-        int answer = 0;
+    public static void dfs(int node){
 
-        return answer;
-    }
-
-    public static void dfs(int n){
-        
-        visited[n] = true;
-        for(int i = 1; i < board.length; i ++){
-            if(!visited[n] && board[n][i] == 1){
-                System.out.println(n + " ");
-                dfs(n);
+        if(n == node)
+            answer ++;
+        else{
+            for(int i = 1; i <= n; i ++){
+                if(!visited[i] && board[node][i] == 1){
+                    visited[i] = true;
+                    dfs(i);
+                    visited[i] = false;
+                }
             }
         }
-
     }
 
 }
