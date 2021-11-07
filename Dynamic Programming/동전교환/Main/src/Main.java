f5111a372d45ac68332144087130f4e5648c897a
bana_na21 <bana_na21@naver.com>
2021-11-07 20:06:16 +0900

    자바 기초 알고리즘 공부 26일차

--- Dynamic Programming/동전교환/Main/src/Main.java
+++ Dynamic Programming/동전교환/Main/src/Main.java
@@ -1,27 +1,48 @@
 import java.io.*;
 import java.util.Arrays;
 public class Main {
+/* 
+    설명
+        다음과 같이 여러 단위의 동전들이 주어져 있을때 거스름돈을 가장 적은 수의 동전으로 교환해주려면 어떻게 주면 되는가?
+        각 단위의 동전은 무한정 쓸 수 있다.
 
+    입력
+        첫 번째 줄에는 동전의 종류개수 N(1<=N<=50)이 주어진다.
+        두 번째 줄에는 N개의 동전의 종류가 주어지고, 그 다음줄에 거슬러 줄 금액 M(1<=M<=500)이 주어진다.
+        각 동전의 종류는 100원을 넘지 않는다.
+
+    출력
+        첫 번째 줄에 거슬러 줄 동전의 최소개수를 출력한다.
+
+    Input :
+        3
+        1 2 5
+        15
+
+    Output :
+        3
+*/
     public static int answer = 0;
-    public static int sum = 0;
-
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] damage = Arrays.stream(br.readLine().split(" ")).mapToInt(item -> Integer.parseInt(item)).toArray();
-        int[] happy = Arrays.stream(br.readLine().split(" ")).mapToInt(item -> Integer.parseInt(item)).toArray();
+        int[] category = Arrays.stream(br.readLine().split(" ")).mapToInt(item -> Integer.parseInt(item)).toArray();
+        int total = Integer.parseInt(br.readLine());
 
-        solution(0, n, 0, 100, damage, happy);
+        int[] arr = new int[total];
+        Arrays.fill(arr, Integer.MAX_VALUE);
+        System.out.println(solution(n, category, arr));
     }
 
-    public static void solution(int depth, int n, int score, int stamina, int[] damage, int[] happy){
-        if(depth == n){
-            System.out.println("happy : " + score + ", damage : " + stamina);
-            answer = (answer > sum) ? answer : sum;
-            return;
-        }else{
-            solution(depth + 1, n, score + happy[depth], stamina - damage[depth], damage, happy);
-            solution(depth + 1, n, score, stamina, damage, happy);
-        }
+    public static int solution(int n, int[] category, int[] arr){
+
+        arr[0] = 1;
+
+        for(int i = 0; i < n; i ++) // 동전의 종류만큼 반복
+            for(int j = category[i]; j < arr.length; j ++) // j의 초기값을 동전 종류의 첫 번째 값으로 설정
+                arr[j] = Math.min(arr[j], arr[j - category[i]] + 1);
+
+        return arr[arr.length - 1];
     }
 }
